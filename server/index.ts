c072336cf41909e953fe7bc226bc534684cbddd6
kbpop <kp.petzold.bradley@gmail.com>
2026-06-28 11:48:33 -0400

    Added the proper server start

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import path from 'path'
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -7,6 +8,14 @@ app.use(express.json())
 
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the Express backend!' })
+})
+
+// Routing to index.html in dist
+const distPath = path.join(process.cwd(), 'dist')
+app.use(express.static(distPath))
+
+app.get('/{*splat}', (req: Request, res: Response) => {
+  res.sendFile(path.join(distPath, 'index.html'))
 })
 
 app.listen(PORT, () => {
