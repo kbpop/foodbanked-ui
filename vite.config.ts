1bf00a72108a68bea49662bf8a7ece2616a342f1
kbpop <kp.petzold.bradley@gmail.com>
2026-07-03 16:22:06 -0400

    Hot reload with docker

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 5173,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
